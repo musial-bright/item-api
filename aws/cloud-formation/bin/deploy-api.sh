3cbbc0ab62ba7efa8dc9a5a6e105636f34d3ebc7
Adam Musial-Bright <adam.musialbright@googlemail.com>
2025-02-09 15:27:56 +0100

    adding deployment script

--- aws/cloud-formation/bin/deploy-api.sh
+++ aws/cloud-formation/bin/deploy-api.sh
@@ -3,11 +3,15 @@
 BASEDIR=$(dirname "$0")
 PWD=$(pwd)
 WORK_DIR="$PWD/$BASEDIR/../../../api"
-BUCKET_NAME="cf-item-api-deployment"
+
+FUNCTION_NAME="item-api-development"
+FUNCTION_ZIP_FILE="$WORK_DIR/dist/index.zip"
 
 cd $WORK_DIR
 npm ci
 npm run build
 cd dist/
 zip index.zip index.cjs
-aws s3 cp index.zip "s3://${BUCKET_NAME}"
+aws lambda update-function-code \
+  --function-name $FUNCTION_NAME \
+  --zip-file fileb://$FUNCTION_ZIP_FILE
