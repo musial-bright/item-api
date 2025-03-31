37bd894e270fae97a2b674e60f45ba17c29795ac
Adam Musial-Bright <adam.musialbright@googlemail.com>
2025-03-31 14:25:58 +0200

    adding table name documentation

--- api/src/utils/tableName.ts
+++ api/src/utils/tableName.ts
@@ -3,6 +3,11 @@ import fastifyConfig from '../config/fastifyConfig'
 
 const stage = currentEnvironemnt()
 
+/**
+ * The tables consist of the following naming structure: `<prefix>-<stage>-<suffix>`
+ * @param tableNameSuffix suffix of the table name which represents the item name
+ * @returns `string` with `<prefix>-<stage>-<suffix>`
+ */
 export const tableName = ({ tableNameSuffix }: { tableNameSuffix: string }) => {
   return `${fastifyConfig.register.prefix}-${stage}-${tableNameSuffix}`
 }
